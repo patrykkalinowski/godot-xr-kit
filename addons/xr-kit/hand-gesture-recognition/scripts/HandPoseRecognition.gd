1eb0240ccff4c29b884cac9e35186f8a3100ead5
Patryk Kalinowski <patryk@patrykkalinowski.com>
2023-04-06 22:13:43 +0200

    first version of hand gesture (poses) recognition

---
+++ addons/xr-kit/hand-gesture-recognition/scripts/HandPoseRecognition.gd
@@ -0,0 +1,46 @@
+extends Node
+
+@export var skeleton: Skeleton3D
+@export var debug_label: Label3D
+
+var templates: Dictionary
+var pose: String
+
+signal new_pose(pose: String, previouse_pose: String)
+
+func _ready():
+	templates = load_templates()
+
+func _physics_process(delta):
+	var recognized_pose: String = recognize_pose()
+	if recognized_pose != pose:
+		new_pose.emit(recognized_pose, pose)
+		pose = recognized_pose
+	
+func recognize_pose():
+	var best_match_key: String
+	var best_match_distance: float = INF
+	
+	for key in templates.keys():
+		var total_distance: float = 0
+		
+		for bone_id in skeleton.get_bone_count():
+			var distance: float = skeleton.get_bone_global_pose(bone_id).origin.distance_to(skeleton.get_bone_global_pose(0).origin)
+			var template_distance: float = templates[key][String.num_int64(bone_id)]			
+			total_distance += abs(distance - template_distance)
+		
+		if total_distance < best_match_distance:
+			best_match_key = key
+			best_match_distance = total_distance
+
+	if debug_label:
+		debug_label.text = best_match_key
+	return best_match_key
+
+func load_templates():
+		var file = FileAccess.open("res://addons/xr-kit/hand-gesture-recognition/hand_pose_templates.json", FileAccess.READ)
+		if file:
+			# TODO: catch error when json is incorrect
+			return JSON.parse_string(file.get_as_text())
+		else:
+			return {}
---
+++ addons/xr-kit/hand-gesture-recognition/scripts/HandPoseSave.gd
@@ -0,0 +1,38 @@
+extends Node
+
+@export var skeleton: Skeleton3D
+@export var pose_name: String
+
+var templates: Dictionary
+
+func _ready():
+	templates = load_templates()
+	
+func _input(event):
+	# press space to save current pose as template
+	if event.as_text() == "Space":
+		save_pose(pose_name)		
+
+# read positions of bones in skeleton and save them to JSON object under a name
+func save_pose(name: String):
+	var template: Dictionary = { name: {} }
+	
+	for bone_id in skeleton.get_bone_count():
+		# distance from current bone to wrist bone
+		template[name][bone_id] = skeleton.get_bone_global_pose(bone_id).origin.distance_to(skeleton.get_bone_global_pose(0).origin)
+	
+	templates.merge(template, true)
+	
+	save_templates(templates)
+
+func save_templates(templates: Dictionary):
+		var file = FileAccess.open("res://addons/xr-kit/hand-gesture-recognition/hand_pose_templates.json", FileAccess.WRITE)
+		var json = JSON.stringify(templates)
+		file.store_string(json)
+
+func load_templates():
+		var file = FileAccess.open("res://addons/xr-kit/hand-gesture-recognition/hand_pose_templates.json", FileAccess.READ)
+		if file:
+			return JSON.parse_string(file.get_as_text())
+		else:
+			return {}
