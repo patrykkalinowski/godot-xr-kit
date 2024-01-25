873d88cc1cd5ef150d827f382471b73b044ecd9e
Patryk Kalinowski <patryk@patrykkalinowski.com>
2024-01-25 23:16:06 +0100

    fixed spinner and head collisions

---
+++ addons/xr-kit/examples/playground/scenes/spinner.gd
@@ -0,0 +1,11 @@
+extends CharacterBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	rotate_y(deg_to_rad(10) * delta)
